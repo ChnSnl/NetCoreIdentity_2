6dc2a44bfd6452dc86aec6aed96173466f706800
chnsenel@gmail.com <chnsenel@gmail.com>
2024-03-15 15:06:55 +0300

    Register işlemleri yapıldı. Create tarafında Admin aktif.

--- NetCoreIdentity_2/Controllers/HomeController.cs
+++ NetCoreIdentity_2/Controllers/HomeController.cs
@@ -1,16 +1,28 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NetCoreIdentity_2.Models;
+using NetCoreIdentity_2.Models.Entities;
+using NetCoreIdentity_2.Models.ViewModels.AppUsers.PureVms;
 using System.Diagnostics;
 
 namespace NetCoreIdentity_2.Controllers
 {
+    [AutoValidateAntiforgeryToken]
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        readonly UserManager<AppUser> _userManager;
+        readonly RoleManager<AppRole> _roleManager;
+        readonly SignInManager<AppUser> _signInManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager)
         {
             _logger = logger;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -28,5 +40,47 @@ namespace NetCoreIdentity_2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Register(UserRegisterRequestModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(appUser, model.Password);
+
+                if (result.Succeeded)
+                {
+                    AppRole role = await _roleManager.FindByNameAsync("Admin");
+                    if (role != null) await _roleManager.CreateAsync(new() { Name = "Admin" });
+                    await _userManager.AddToRoleAsync(appUser, "Admin");
+
+                    /*------------------Admin / Member----------------------*/
+
+                    //AppRole role = await _roleManager.FindByNameAsync("Member");
+                    //if (role != null) await _roleManager.CreateAsync(new() { Name = "Member" });
+                    //await _userManager.AddToRoleAsync(appUser, "Member");
+                    return RedirectToAction("Register");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+
     }
 }
---
+++ NetCoreIdentity_2/Models/ViewModels/AppUsers/PureVms/UserRegisterRequestModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreIdentity_2.Models.ViewModels.AppUsers.PureVms
+{
+    public class UserRegisterRequestModel
+    {
+        [Required(ErrorMessage ="{0} girişi zorunludur.")]
+        [Display(Name="Kullanıcı İsmi")]
+        public string UserName { get; set; }
+
+        [EmailAddress(ErrorMessage ="Email formatında giriş yapınız")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="{0} girişi zorunludur.")]
+        [MinLength(3,ErrorMessage ="Minimum {0} karakter girilmesi zorunludur.")]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Parolalar uyuşmuyor.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
